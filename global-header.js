d417fbc105e459a8a77272c596e5afcfdd7ebab3
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-04-24 15:29:44 -0400

    Update global-header.js

--- global-header.js
+++ global-header.js
@@ -4,6 +4,6 @@
 
 const = INJECT = '  <div id="global-preheader"><div class="bordered"><marquee><h1>Kipton\'s Home Page😎</h1></marquee></div></div><div id="global-header"><ul id="nav-list"><li><a href="/hymiller">Home</a></li><li>Photos<ul><li><a href="/hymiller/photo-albums/index.html">no longer filler</a></li></ul></li><li>Videos<ul><li><a href="#">filler</a></li></ul></li><li>Personal<ul><li><a href="#">filler</a></li></ul></li><li><a href="/hymiller/web-dev">Web Dev</a><ul><li><a href="../wdsub/week1.html">Week 1</a></li><li><a href="../wdsub/recipe.html">Week 2</a></li><li><a href="../wdsub/anchors.html">Week 3</a></li><li><a href="../wdsub/poem.html">Week 4</a></li><li><a href="../wdsub/review.html">Week 5</a></li><li><a href="../wdsub/Ocean-Occurrences.html">Week 6</a></li><li><a href="../wdsub/band-page.html">Week 7</a></li><li><a href="../wdsub/to-do-list.html">Week 8</a></li><li><a href="#">Week 9</a></li><li><a href="#">Week 10</a></li></ul></li></ul></div>';
 
-let _div = ducument.getElementById("gheader");
+let _div = document.getElementById("gheader");
 
 _div.innerHTML = INJECT;
