d4d974dfe2bf6523b383469986bd68250f093b6a
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-05-12 15:30:13 -0400

    Update ttt.js

--- wdsub/ttt.js
+++ wdsub/ttt.js
@@ -1,1 +1,30 @@
+let isXTurn = true;
+let squareArr = document.querySelectorAll(".square");
 
+for(const _element of squareArr) {
+  _element.addEventListener("click", markSquare);
+}
+
+function markSquare(_square) {
+  if (_square.srcElement.innerText != "x" && _square.srcElement.innerText != "o") {
+  if (isXTurn) {
+    _square.srcElement.innerText = "x";
+  } else {
+    _square.srcElement.innerText = "o";
+  }
+  
+  checkComplete();
+    
+  isXTurn = !isXTurn;
+  }
+}
+
+function checkComplete() {
+  for (let _i = 0; _i < 3; _i++) {
+    if ((squareArr[_i].innerText == "x" || squareArr[_i].innerText == "o" )
+      && squareArr[_i].innerText == squareArr[_i + 3].innerText
+        && squareArr[_i].innerText == squareArr[_i + 6].innerText
+      )console.log("the game is over")
+      
+  }
+}
